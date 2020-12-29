b9429ed10fd96f788ca380aa7fa68c833c021f39
Dan Turner <djtech42@me.com>
2020-12-29 15:10:03 -0500

    Update fish_greeting for Fish 3.0 (#4)
    * Update fish_greeting for Fish 3.0
    
    * fix syntax

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -9,7 +9,7 @@
 #
 
 function fish_greeting -d "what's up, fish?"
-    if math "$LINES >= 20 && $COLUMNS >= 80" > /dev/null
+    if test $LINES -ge 20 -a $COLUMNS -ge 80 > /dev/null
         bash (dirname (status -f))/archey.sh
     end
 end
